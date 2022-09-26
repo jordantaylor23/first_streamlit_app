3ba88ce8900ba8e3720160dfc4d553d87145ffc8
Jordan Taylor <96889671+jordantaylor23@users.noreply.github.com>
2022-09-26 10:55:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 # After adding cffi and pyarrow in the other file
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
